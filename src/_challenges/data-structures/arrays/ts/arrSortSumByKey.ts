fa0afb224723b43ddd612de4db731e603eb0045b
Derek R. Miller <20782161+DerekRMiller@users.noreply.github.com>
2022-01-16 16:42:44 -0800

    Adding maximumBinaryTree and updating arrSortSumByKey (#6)

--- src/_challenges/data-structures/arrays/ts/arrSortSumByKey.ts
+++ src/_challenges/data-structures/arrays/ts/arrSortSumByKey.ts
@@ -1,18 +1,25 @@
 /* 
-  In a key value string array, sort and sum by key:
+  In a key and number value string array, sort and sum by key:
   const arr = ['a:3', 'd:-2', 'f:4', 'e:1', 'c:9', 'b:0', 'c:7', 'd:5', 'b:12', 'f:-23'];
   const newArr = ['a:3', 'b:12', 'c:16', 'd:3', 'e:1', 'f:-19'];
 */
 
 import util = require('util');
 
+const notKeyNumberValuePair =
+  'Array string elements must be formatted as a key and number value pair.';
+
 const arrSortSumByKey = (arr: string[]) => {
   const obj: { [key: string]: number } = {};
   const newArr: string[] = [];
 
   for (let i = 0; i < arr.length; i++) {
+    if (arr[i].match('(:.*){2}')) return notKeyNumberValuePair;
+
     const pair = arr[i].split(':');
     const value = parseInt(pair[1]);
+
+    if (isNaN(value)) return notKeyNumberValuePair;
 
     if (pair[0] in obj) {
       obj[pair[0]] += value;
@@ -47,5 +54,5 @@ Sorted and summed by key
 ------------------------------------------------
 ${util.inspect(arrSort, false, null, true)}
 ------------------------------------------------
-`);
+  `);
 };
